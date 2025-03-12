10e56ebfa940728a79417de1cabea4efb861da68
NguyenNhuTu09 <frankenstein1909@gmail.com>
2025-03-12 12:45:05 +0700

    setup project, react native with android simulator

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi Software Enginner (Level Intern) Nguyen Nhu Tu</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
